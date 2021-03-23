dec643dd22007976972f2df5f680c764cb0be6dd
e9833711_오범석 <lawsnland@gmail.com>
2021-03-23 17:26:10 +0900

    myboot

--- src/main/java/com/doo/myboot/MybootApplication.java
+++ src/main/java/com/doo/myboot/MybootApplication.java
@@ -2,18 +2,35 @@ package com.doo.myboot;
 
 import com.doo.myboot.demojpa.Customer;
 import com.doo.myboot.demojpa.CustomerRepository;
+import com.doo.myboot.demoredis.Receiver;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.core.StringRedisTemplate;
+import org.springframework.data.redis.listener.PatternTopic;
+import org.springframework.data.redis.listener.RedisMessageListenerContainer;
+import org.springframework.data.redis.listener.adapter.MessageListenerAdapter;
 
 @SpringBootApplication
 @Slf4j
 public class MybootApplication {
 
-    public static void main(String[] args) {
-        SpringApplication.run(MybootApplication.class, args);
+    public static void main(String[] args) throws InterruptedException {
+        ApplicationContext ctx = SpringApplication.run(MybootApplication.class, args);
+
+        StringRedisTemplate template = ctx.getBean(StringRedisTemplate.class);
+        Receiver receiver = ctx.getBean(Receiver.class);
+
+        while (receiver.getCount() == 0) {
+            log.info("Sending message...");
+            template.convertAndSend("chat", "Hello from Redis!");
+            Thread.sleep(500L);
+        }
+        System.exit(0);
     }
 
     @Bean
@@ -52,5 +69,29 @@ public class MybootApplication {
 //            }
             log.info("");
         };
+    }
+
+    @Bean
+    RedisMessageListenerContainer container(RedisConnectionFactory connectionFactory
+            , MessageListenerAdapter listenerAdapter) {
+        RedisMessageListenerContainer container = new RedisMessageListenerContainer();
+        container.setConnectionFactory(connectionFactory);
+        container.addMessageListener(listenerAdapter, new PatternTopic("chat"));
+        return container;
+    }
+
+    @Bean
+    MessageListenerAdapter listenerAdapter(Receiver receiver) {
+        return new MessageListenerAdapter(receiver, "receiveMessage");
+    }
+
+    @Bean
+    Receiver receiver() {
+        return new Receiver();
+    }
+
+    @Bean
+    StringRedisTemplate template(RedisConnectionFactory connectionFactory) {
+        return new StringRedisTemplate(connectionFactory);
     }
 }
---
+++ src/test/java/javatest/FileTest.java
@@ -0,0 +1,25 @@
+package javatest;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+
+public class FileTest {
+
+    public static void main(String[] args) throws FileNotFoundException, IOException {
+
+        String fileName = "/home/janbodnar/tmp/smallfile.txt";
+
+        try (FileInputStream fis = new FileInputStream(fileName)) {
+
+            char c1 = (char) fis.read();
+            char c2 = (char) fis.read();
+            char c3 = (char) fis.read();
+
+            System.out.println(c1);
+            System.out.println(c2);
+            System.out.println(c3);
+        }
+
+    }
+}
